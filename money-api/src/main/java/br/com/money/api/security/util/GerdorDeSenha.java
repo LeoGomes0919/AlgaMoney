4ba7d859b9639ca948ee4c318ef7df7da7f7ecb6
'Leonardo' <'leonardogs09192@gmail.com'>
2019-09-21 11:01:51 -0300

    Movendo usuário para banco de dodos.

---
+++ money-api/src/main/java/br/com/money/api/security/util/GerdorDeSenha.java
@@ -0,0 +1,11 @@
+package br.com.money.api.security.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GerdorDeSenha {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder enconder = new BCryptPasswordEncoder();
+		System.out.println(enconder.encode("leoGomes"));
+	}
+}
