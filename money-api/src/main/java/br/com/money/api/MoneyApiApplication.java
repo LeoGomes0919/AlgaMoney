5aa49a144be8fc3f1906444501d2db1bceb31261
'Leonardo' <'leonardogs09192@gmail.com'>
2019-09-22 21:20:14 -0300

    Profiles do Spring

--- money-api/src/main/java/br/com/money/api/MoneyApiApplication.java
+++ money-api/src/main/java/br/com/money/api/MoneyApiApplication.java
@@ -2,8 +2,12 @@ package br.com.money.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.com.money.api.config.property.MoneyApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MoneyApiProperty.class)
 public class MoneyApiApplication {
 
 	public static void main(String[] args) {
