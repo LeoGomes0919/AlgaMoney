77e8bd3247d7a8d9f669422f92a60e33a897cc34
'Leonardo' <'leonardogs09192@gmail.com'>
2019-11-11 21:33:47 -0300

    Gerando bytes do relatório.

--- money-api/src/main/java/br/com/money/api/service/LancamentoService.java
+++ money-api/src/main/java/br/com/money/api/service/LancamentoService.java
@@ -1,16 +1,29 @@
 package br.com.money.api.service;
 
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.money.api.dto.LancamentoEstatisticaPessoa;
 import br.com.money.api.model.Lancamento;
 import br.com.money.api.model.Pessoa;
 import br.com.money.api.repository.LancamentoRepository;
 import br.com.money.api.repository.PessoaRepository;
 import br.com.money.api.service.exception.PessoaInexistenteOuInativaExcpetion;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 @Service
 public class LancamentoService {
@@ -20,6 +33,23 @@ public class LancamentoService {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+
+	public byte[] relatorioPorPessoa(LocalDate inicio, LocalDate fim) throws Exception {
+		List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(inicio, fim);
+
+		Map<String, Object> parametros = new HashMap<>();
+
+		parametros.put("DT_INICIO", Date.valueOf(inicio));
+		parametros.put("DT_FIM", Date.valueOf(fim));
+		parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+		InputStream inputStream = this.getClass().getResourceAsStream("/relatorios/lancamentosPorPessoa.jasper");
+
+		JasperPrint print = JasperFillManager.fillReport(inputStream, parametros,
+				new JRBeanCollectionDataSource(dados));
+
+		return JasperExportManager.exportReportToPdf(print);
+	}
 
 	public Lancamento salvar(@Valid Lancamento lancamento) {
 		Pessoa pessoa = pessoaRepository.findById(lancamento.getPessoa().getCodigo()).orElse(null);
